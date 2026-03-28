331947251e06ab4b50cdc539cc3c6ebaa6104bc5
Lothar Rubusch <l.rubusch@gmail.com>
2026-03-28 19:16:24 +0100

    client: make error a bit more helpful
    If a Place has multiple SSH targets, typing "labgrid-client ssh <net-name>"
    triggers an ambiguous "multiple targets" error. It’s usually just a missing
    -n flag, so let's point the user in the right direction with a clearer hint
    in the logs.
    
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -1234,6 +1234,10 @@ class ClientSession:
             return drv
 
     def ssh(self):
+        if self.args.leftover and not self.args.name:
+            potential_name = self.args.leftover[0]
+            print(f"Unknown argument '{potential_name}', did you mean 'labgrid-client ssh -n {potential_name}'?")
+
         drv = self._get_ssh()
 
         res = drv.interact(self.args.leftover)
