4090da6198f9b46b009153a14c838dbb1bc6be87
Lothar Rubusch <l.rubusch@gmail.com>
2026-04-08 22:48:28 +0200

    client: scp - add globbing
    Adds file globbing, e.g. writing `*.txt` to copy multiple files at once.
    
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- labgrid/remote/client.py
+++ labgrid/remote/client.py
@@ -1272,12 +1272,20 @@ class ClientSession:
 
     def scp(self):
         drv = self._get_ssh()
-
-        res = drv.scp(src=self.args.src, dst=self.args.dst)
-        if res:
-            exc = InteractiveCommandError("scp error")
-            exc.exitcode = res
-            raise exc
+        import glob
+        sources = []
+        for s in self.args.src:
+            expanded = glob.glob(s)
+            if expanded:
+                sources.extend(expanded)
+            else:
+                sources.append(s)
+        for src_file in sources:
+            res = drv.scp(src=src_file, dst=self.args.dst)
+            if res:
+                exc = InteractiveCommandError("scp error")
+                exc.exitcode = res
+                raise exc
 
     def rsync(self):
         drv = self._get_ssh()
@@ -1949,7 +1957,7 @@ def main():
 
     subparser = subparsers.add_parser("scp", help="transfer file via scp")
     subparser.add_argument("--name", "-n", help="optional resource name")
-    subparser.add_argument("src", help="source path (use :dir/file for remote side)")
+    subparser.add_argument("src", nargs='+', help="source path(s) (use :dir/file for remote side)")
     subparser.add_argument("dst", help="destination path (use :dir/file for remote side)")
     subparser.set_defaults(func=ClientSession.scp)
 
